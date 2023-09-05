61b12785bd572229e2ec463d7a9366bb2eb6ae50
sielinou <diderotsielinou@gmail.com>
2023-09-05 18:55:08 +0200

    initialisation et installation des packages

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# HotelProjet
+# ProjetRestaurant
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.1.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hotelProjet'`, () => {
+  it(`should have as title 'projetRestaurant'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hotelProjet');
+    expect(app.title).toEqual('projetRestaurant');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hotelProjet app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('projetRestaurant app is running!');
   });
 });
